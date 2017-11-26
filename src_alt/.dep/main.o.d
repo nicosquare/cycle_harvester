b15940ccc4ff7b76b1437746dbb9d39eb6e064b9
Nicolas Cuadrado <nicolas.cuadrado@hotmail.com>
2017-11-26 15:00:55 -0500

    Alt project with Chibios 16.1.8

---
+++ src_alt/.dep/main.o.d
@@ -0,0 +1,263 @@
+build/obj/main.o: main.c ../../ChibiOS_16.1.8/os/rt/include/ch.h \
+ ../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/compilers/GCC/chtypes.h \
+ /home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stddef.h \
+ /home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdint.h \
+ /home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/stdint.h \
+ /home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdbool.h \
+ chconf.h ../../ChibiOS_16.1.8/os/rt/include/chlicense.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chsystypes.h \
+ ../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore.h \
+ ../../ChibiOS_16.1.8/os/common/ports/ARMCMx/devices/STM32F1xx/cmparams.h \
+ ../../ChibiOS_16.1.8/os/hal/boards/ST_STM32VL_DISCOVERY/board.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/stm32f1xx.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/stm32f100xb.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cm3.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cmInstr.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cmFunc.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/system_stm32f1xx.h \
+ ../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore_v7m.h \
+ ../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore_timer.h \
+ ../../ChibiOS_16.1.8/os/hal/include/st.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/TIMv1/st_lld.h mcuconf.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_registry.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chdebug.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chtm.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chstats.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chschd.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chsys.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chvt.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chthreads.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chregistry.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chsem.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chbsem.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmtx.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chcond.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chevents.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmsg.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmboxes.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmemcore.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chheap.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmempools.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chdynamic.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chqueues.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chstreams.h \
+ ../../ChibiOS_16.1.8/os/hal/include/hal.h \
+ ../../ChibiOS_16.1.8/os/hal/osal/rt/osal.h halconf.h mcuconf.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/hal_lld.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_registry.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/hal_lld_f100.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/common/ARMCMx/nvic.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_isr.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/DMAv1/stm32_dma.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_rcc.h \
+ ../../ChibiOS_16.1.8/os/hal/include/hal_streams.h \
+ ../../ChibiOS_16.1.8/os/hal/include/hal_channels.h \
+ ../../ChibiOS_16.1.8/os/hal/include/hal_files.h \
+ ../../ChibiOS_16.1.8/os/hal/include/hal_ioblock.h \
+ ../../ChibiOS_16.1.8/os/hal/include/hal_mmcsd.h \
+ ../../ChibiOS_16.1.8/os/hal/include/hal_buffers.h \
+ ../../ChibiOS_16.1.8/os/hal/include/hal_queues.h \
+ ../../ChibiOS_16.1.8/os/hal/include/pal.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/GPIOv1/pal_lld.h \
+ ../../ChibiOS_16.1.8/os/hal/include/adc.h \
+ ../../ChibiOS_16.1.8/os/hal/include/can.h \
+ ../../ChibiOS_16.1.8/os/hal/include/dac.h \
+ ../../ChibiOS_16.1.8/os/hal/include/ext.h \
+ ../../ChibiOS_16.1.8/os/hal/include/gpt.h \
+ ../../ChibiOS_16.1.8/os/hal/include/i2c.h \
+ ../../ChibiOS_16.1.8/os/hal/include/i2s.h \
+ ../../ChibiOS_16.1.8/os/hal/include/icu.h \
+ ../../ChibiOS_16.1.8/os/hal/include/mac.h \
+ ../../ChibiOS_16.1.8/os/hal/include/mii.h \
+ ../../ChibiOS_16.1.8/os/hal/include/pwm.h \
+ ../../ChibiOS_16.1.8/os/hal/include/rtc.h \
+ ../../ChibiOS_16.1.8/os/hal/include/serial.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/USARTv1/serial_lld.h \
+ ../../ChibiOS_16.1.8/os/hal/include/sdc.h \
+ ../../ChibiOS_16.1.8/os/hal/include/spi.h \
+ ../../ChibiOS_16.1.8/os/hal/include/uart.h \
+ ../../ChibiOS_16.1.8/os/hal/include/usb.h \
+ ../../ChibiOS_16.1.8/os/hal/include/wdg.h \
+ ../../ChibiOS_16.1.8/os/hal/include/st.h \
+ ../../ChibiOS_16.1.8/os/hal/include/mmc_spi.h \
+ ../../ChibiOS_16.1.8/os/hal/include/serial_usb.h \
+ ../../ChibiOS_16.1.8/test/rt/test.h
+
+../../ChibiOS_16.1.8/os/rt/include/ch.h:
+
+../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/compilers/GCC/chtypes.h:
+
+/home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stddef.h:
+
+/home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdint.h:
+
+/home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/stdint.h:
+
+/home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdbool.h:
+
+chconf.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chlicense.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chsystypes.h:
+
+../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore.h:
+
+../../ChibiOS_16.1.8/os/common/ports/ARMCMx/devices/STM32F1xx/cmparams.h:
+
+../../ChibiOS_16.1.8/os/hal/boards/ST_STM32VL_DISCOVERY/board.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/stm32f1xx.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/stm32f100xb.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cm3.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cmInstr.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cmFunc.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/system_stm32f1xx.h:
+
+../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore_v7m.h:
+
+../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore_timer.h:
+
+../../ChibiOS_16.1.8/os/hal/include/st.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/TIMv1/st_lld.h:
+
+mcuconf.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_registry.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chdebug.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chtm.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chstats.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chschd.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chsys.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chvt.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chthreads.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chregistry.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chsem.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chbsem.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmtx.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chcond.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chevents.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmsg.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmboxes.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmemcore.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chheap.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmempools.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chdynamic.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chqueues.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chstreams.h:
+
+../../ChibiOS_16.1.8/os/hal/include/hal.h:
+
+../../ChibiOS_16.1.8/os/hal/osal/rt/osal.h:
+
+halconf.h:
+
+mcuconf.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/hal_lld.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_registry.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/hal_lld_f100.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/common/ARMCMx/nvic.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_isr.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/DMAv1/stm32_dma.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_rcc.h:
+
+../../ChibiOS_16.1.8/os/hal/include/hal_streams.h:
+
+../../ChibiOS_16.1.8/os/hal/include/hal_channels.h:
+
+../../ChibiOS_16.1.8/os/hal/include/hal_files.h:
+
+../../ChibiOS_16.1.8/os/hal/include/hal_ioblock.h:
+
+../../ChibiOS_16.1.8/os/hal/include/hal_mmcsd.h:
+
+../../ChibiOS_16.1.8/os/hal/include/hal_buffers.h:
+
+../../ChibiOS_16.1.8/os/hal/include/hal_queues.h:
+
+../../ChibiOS_16.1.8/os/hal/include/pal.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/GPIOv1/pal_lld.h:
+
+../../ChibiOS_16.1.8/os/hal/include/adc.h:
+
+../../ChibiOS_16.1.8/os/hal/include/can.h:
+
+../../ChibiOS_16.1.8/os/hal/include/dac.h:
+
+../../ChibiOS_16.1.8/os/hal/include/ext.h:
+
+../../ChibiOS_16.1.8/os/hal/include/gpt.h:
+
+../../ChibiOS_16.1.8/os/hal/include/i2c.h:
+
+../../ChibiOS_16.1.8/os/hal/include/i2s.h:
+
+../../ChibiOS_16.1.8/os/hal/include/icu.h:
+
+../../ChibiOS_16.1.8/os/hal/include/mac.h:
+
+../../ChibiOS_16.1.8/os/hal/include/mii.h:
+
+../../ChibiOS_16.1.8/os/hal/include/pwm.h:
+
+../../ChibiOS_16.1.8/os/hal/include/rtc.h:
+
+../../ChibiOS_16.1.8/os/hal/include/serial.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/USARTv1/serial_lld.h:
+
+../../ChibiOS_16.1.8/os/hal/include/sdc.h:
+
+../../ChibiOS_16.1.8/os/hal/include/spi.h:
+
+../../ChibiOS_16.1.8/os/hal/include/uart.h:
+
+../../ChibiOS_16.1.8/os/hal/include/usb.h:
+
+../../ChibiOS_16.1.8/os/hal/include/wdg.h:
+
+../../ChibiOS_16.1.8/os/hal/include/st.h:
+
+../../ChibiOS_16.1.8/os/hal/include/mmc_spi.h:
+
+../../ChibiOS_16.1.8/os/hal/include/serial_usb.h:
+
+../../ChibiOS_16.1.8/test/rt/test.h:
---
+++ src_alt/.dep/testbmk.o.d
@@ -0,0 +1,140 @@
+build/obj/testbmk.o: ../../ChibiOS_16.1.8/test/rt/testbmk.c \
+ ../../ChibiOS_16.1.8/os/rt/include/ch.h \
+ ../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/compilers/GCC/chtypes.h \
+ /home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stddef.h \
+ /home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdint.h \
+ /home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/stdint.h \
+ /home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdbool.h \
+ chconf.h ../../ChibiOS_16.1.8/os/rt/include/chlicense.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chsystypes.h \
+ ../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore.h \
+ ../../ChibiOS_16.1.8/os/common/ports/ARMCMx/devices/STM32F1xx/cmparams.h \
+ ../../ChibiOS_16.1.8/os/hal/boards/ST_STM32VL_DISCOVERY/board.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/stm32f1xx.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/stm32f100xb.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cm3.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cmInstr.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cmFunc.h \
+ ../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/system_stm32f1xx.h \
+ ../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore_v7m.h \
+ ../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore_timer.h \
+ ../../ChibiOS_16.1.8/os/hal/include/st.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/TIMv1/st_lld.h mcuconf.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_registry.h \
+ ../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chdebug.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chtm.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chstats.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chschd.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chsys.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chvt.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chthreads.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chregistry.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chsem.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chbsem.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmtx.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chcond.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chevents.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmsg.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmboxes.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmemcore.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chheap.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chmempools.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chdynamic.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chqueues.h \
+ ../../ChibiOS_16.1.8/os/rt/include/chstreams.h \
+ ../../ChibiOS_16.1.8/test/rt/test.h
+
+../../ChibiOS_16.1.8/os/rt/include/ch.h:
+
+../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/compilers/GCC/chtypes.h:
+
+/home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stddef.h:
+
+/home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdint.h:
+
+/home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/../../../../arm-none-eabi/include/stdint.h:
+
+/home/madog/programs/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin/../lib/gcc/arm-none-eabi/4.6.1/include/stdbool.h:
+
+chconf.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chlicense.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chsystypes.h:
+
+../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore.h:
+
+../../ChibiOS_16.1.8/os/common/ports/ARMCMx/devices/STM32F1xx/cmparams.h:
+
+../../ChibiOS_16.1.8/os/hal/boards/ST_STM32VL_DISCOVERY/board.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/stm32f1xx.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/stm32f100xb.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cm3.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cmInstr.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/include/core_cmFunc.h:
+
+../../ChibiOS_16.1.8/os/ext/CMSIS/ST/STM32F1xx/system_stm32f1xx.h:
+
+../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore_v7m.h:
+
+../../ChibiOS_16.1.8/os/rt/ports/ARMCMx/chcore_timer.h:
+
+../../ChibiOS_16.1.8/os/hal/include/st.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/TIMv1/st_lld.h:
+
+mcuconf.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/STM32F1xx/stm32_registry.h:
+
+../../ChibiOS_16.1.8/os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chdebug.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chtm.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chstats.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chschd.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chsys.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chvt.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chthreads.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chregistry.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chsem.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chbsem.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmtx.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chcond.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chevents.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmsg.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmboxes.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmemcore.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chheap.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chmempools.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chdynamic.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chqueues.h:
+
+../../ChibiOS_16.1.8/os/rt/include/chstreams.h:
+
+../../ChibiOS_16.1.8/test/rt/test.h:
