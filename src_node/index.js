b12e29edefb117536dba25d7a714f85a0bb33391
Nicolas Cuadrado <nicolas.cuadrado@hotmail.com>
2018-02-07 11:48:05 -0500

    Changes on interface to test electric measures

--- src_node/index.js
+++ src_node/index.js
@@ -10,7 +10,7 @@ const sensor = new ina219.sensor(ina219.INA219_ADDRESS);
 
 const server = http.createServer((req, res) => {
 
-	if (req.url === '/?LED=ON'){
+	if (req.url === '/?INFO=GET'){
 		
 		var shuntvoltage = 0;
 		var busvoltage = 0;
@@ -33,13 +33,22 @@ const server = http.createServer((req, res) => {
 		
 	}
 	
-	if (req.url === '/?LED=OFF') exec('fast-gpio set 1 0');
+	if (req.url === '/?INFO=UPDATE'){
+		
+		console.log("Electric measures updated");
+			
+		sensor.getBusVoltage_raw(sensor.updateBusVoltage_raw.bind(sensor));
+		sensor.getShuntVoltage_raw(sensor.updateShuntVoltage_raw.bind(sensor));
+		sensor.getCurrent_raw(sensor.updateCurrent_raw.bind(sensor));
+		sensor.getPower_raw(sensor.updatePower_raw.bind(sensor));
+	
+	}
 	
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
-	res.write('<h2>A simple LED control server <br>with Onion Omega2+</h2>');
-	res.write('<p>LED state: <a href="?LED=ON"> <button>ON</button></a>');
-	res.write('  <a href="?LED=OFF"><button>OFF</button></a></p>');
+	res.write('<h2> A simple test program for Cycle Harvester<br>with Onion Omega2+</h2>');
+	res.write('<p>Get electric measures: <a href="?INFO=GET"> <button>Get</button></a>');
+	res.write('  <a href="?INFO=UPDATE"><button>Update</button></a></p>');
 	res.end();
 });
 
